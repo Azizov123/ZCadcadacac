bb1b5b9433707c5655ba603221dd061b47b5c90f
Azizov123 <144803080+Azizov123@users.noreply.github.com>
2023-09-19 10:08:52 +0300

    Add files via upload

---
+++ 3327_А_Азизов/css/css.js
@@ -0,0 +1,852 @@
+/* Frame 35276 */
+
+width: 1920px;
+height: 88px;
+
+/* Primaty/Grey Light */
+background: #F6F5F4;
+box-shadow: inset 0px -1px 0px #E0E0E0;
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
+
+
+/* Frame 4 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+padding: 0px;
+gap: 207.2px;
+
+position: absolute;
+width: 1180px;
+height: 40px;
+left: 370px;
+top: 24px;
+
+border-radius: 57.8234px;
+
+
+/* Frame 9 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 0px;
+gap: 30px;
+
+margin: 0 auto;
+width: 540px;
+height: 22px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* GlobalTalk */
+
+width: 130px;
+height: 21px;
+
+font-family: 'Dela Gothic One';
+font-style: normal;
+font-weight: 400;
+font-size: 20.8609px;
+line-height: 100%;
+/* identical to box height, or 21px */
+letter-spacing: -0.02em;
+
+color: #000000;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Vector 1 */
+
+width: 0px;
+height: 16.87px;
+
+/* Primaty/Grey Medium */
+border: 1.20466px solid #E0E0E0;
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+
+/* Home */
+
+width: 49px;
+height: 22px;
+
+/* Text/Body 2 */
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 140%;
+/* or 22px */
+letter-spacing: -0.04em;
+
+color: #000000;
+
+
+/* Inside auto layout */
+flex: none;
+order: 2;
+flex-grow: 0;
+
+
+/* Tutorial */
+
+width: 72px;
+height: 22px;
+
+/* Text/Body 2 */
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 140%;
+/* or 22px */
+letter-spacing: -0.04em;
+
+color: #757575;
+
+
+/* Inside auto layout */
+flex: none;
+order: 3;
+flex-grow: 0;
+
+
+/* Statistics */
+
+width: 70px;
+height: 22px;
+
+/* Text/Body 2 */
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 140%;
+/* or 22px */
+letter-spacing: -0.04em;
+
+color: #757575;
+
+
+/* Inside auto layout */
+flex: none;
+order: 4;
+flex-grow: 0;
+
+
+/* Frame 3 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 0px;
+
+width: 69px;
+height: 22px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 5;
+flex-grow: 0;
+
+
+/* Games */
+
+width: 55px;
+height: 22px;
+
+/* Text/Body 2 */
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 140%;
+/* or 22px */
+letter-spacing: -0.04em;
+
+color: #757575;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Frame 8 */
+
+width: 14px;
+height: 14px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+
+/* Vector 2 */
+
+position: absolute;
+width: 6.92px;
+height: 3.46px;
+left: 3.76px;
+top: 6.61px;
+
+border: 1.6px solid #757575;
+
+
+/* Frame 8 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 0px;
+gap: 30px;
+
+margin: 0 auto;
+width: 170px;
+height: 40px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+
+/* Frame 14 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 0px;
+gap: 10px;
+
+width: 85px;
+height: 40px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Frame 2 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 9px 14px;
+gap: 14px;
+
+width: 41px;
+height: 40px;
+
+/* Primaty/Cyan Light */
+background: #C3DCE3;
+border-radius: 100px;
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* A */
+
+width: 13px;
+height: 22px;
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 140%;
+/* or 22px */
+display: flex;
+align-items: center;
+text-align: center;
+letter-spacing: -0.04em;
+
+color: #2B788B;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Alex */
+
+width: 34px;
+height: 22px;
+
+/* Text/Body 2 */
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 600;
+font-size: 16px;
+line-height: 140%;
+/* or 22px */
+letter-spacing: -0.04em;
+
+color: #757575;
+
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+
+/* Выход → */
+
+width: 55px;
+height: 16px;
+
+/* Button/Button 2 */
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 100%;
+/* identical to box height, or 16px */
+letter-spacing: -0.04em;
+
+color: #000000;
+
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+ 
+/* Frame 18 */
+
+width: 1920px;
+height: 740px;
+
+/* Primaty/Grey Light */
+background: #F6F5F4;
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+align-self: stretch;
+flex-grow: 0;
+
+
+/* Frame 8 */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: flex-start;
+padding: 0px;
+gap: 90px;
+
+position: absolute;
+width: 1180px;
+height: 700px;
+left: 369.95px;
+top: 0px;
+
+
+
+/* Frame 5 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+align-items: center;
+padding: 0px;
+gap: 37.8px;
+
+width: 1180px;
+height: 397px;
+
+border-radius: 58.5321px;
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+align-self: stretch;
+flex-grow: 0;
+
+
+/* Frame 4 */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 48.78px;
+
+margin: 0 auto;
+width: 531px;
+height: 397px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Frame 4 */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 32px;
+
+width: 531px;
+height: 397px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Frame 4 */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 24px;
+
+width: 500px;
+height: 321px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* E-COURSE PLATFORM */
+
+width: 221px;
+height: 16px;
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 100%;
+/* identical to box height, or 16px */
+letter-spacing: 0.14em;
+text-transform: uppercase;
+
+/* Primaty/Cyan Dark */
+color: #2B788B;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Frame 5 */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 30px;
+
+width: 531px;
+height: 281px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+
+/* Learning and teaching online, made easy. */
+
+width: 531px;
+height: 201px;
+
+/* Headings/Heading 1 */
+font-family: 'Dela Gothic One';
+font-style: normal;
+font-weight: 400;
+font-size: 56px;
+line-height: 120%;
+/* or 67px */
+letter-spacing: -0.04em;
+
+/* Primaty/Black */
+color: #000000;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Practice your English and learn new things with the platform. */
+
+width: 396.89px;
+height: 50px;
+
+/* Text/Body 1 */
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 600;
+font-size: 18px;
+line-height: 140%;
+/* or 25px */
+letter-spacing: -0.04em;
+
+/* Primaty/Grey Dark */
+color: #757575;
+
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+
+/* Frame 7 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+align-items: center;
+padding: 0px;
+gap: 30px;
+
+width: 165px;
+height: 44px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+
+/* Frame 2 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 14px 22px;
+gap: 19px;
+
+width: 165px;
+height: 44px;
+
+/* Primaty/Cyan Light */
+background: #C3DCE3;
+border-radius: 100px;
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* About platform */
+
+width: 121px;
+height: 16px;
+
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 100%;
+/* identical to box height, or 16px */
+display: flex;
+align-items: center;
+letter-spacing: -0.04em;
+
+color: #2B788B;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Group 4 */
+
+margin: 0 auto;
+width: nanpx;
+height: nanpx;
+
+transform: matrix(1, 0, 0, 1, nan, nan);
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+
+/* casual-life-3d-old-game-controller 8 */
+
+position: absolute;
+visibility: hidden;
+width: 282px;
+height: 173px;
+left: 974px;
+top: 1308.5px;
+
+background: url(casual-life-3d-old-game-controller.png);
+
+
+/* Frame 24 */
+
+position: absolute;
+width: 933px;
+height: 601px;
+left: 870.05px;
+top: 109px;
+
+
+
+/* image 6 */
+
+position: absolute;
+width: 143px;
+height: 171px;
+left: 15.5px;
+top: 187px;
+
+background: url(image.png);
+filter: blur(7px);
+
+
+/* casual-life-3d-boy-sitting-at-the-desk-with-open-book 9 */
+
+position: absolute;
+width: 608.73px;
+height: 507.28px;
+left: 51.77px;
+top: 0px;
+
+background: url(6971ac74-78f0-49f4-b019-d4cc31a5b31c.png);
+
+
+/* image 12 */
+
+position: absolute;
+width: 536px;
+height: 571px;
+left: 396.5px;
+top: 30px;
+
+background: url(image.png);
+filter: blur(10.5px);
+ 
+/* Frame 21 */
+
+width: 1920px;
+height: 600px;
+
+background: #FFFFFF;
+
+/* Inside auto layout */
+flex: none;
+order: 3;
+align-self: stretch;
+flex-grow: 0;
+
+
+/* image 1 */
+
+position: absolute;
+width: 400px;
+height: 409.25px;
+left: 370px;
+top: 95.37px;
+
+background: url(image.png);
+
+
+/* Frame 8 */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 34px;
+
+position: absolute;
+width: 474.46px;
+height: 302px;
+left: 1070px;
+top: 149px;
+
+
+
+/* Frame 5 */
+
+/* Auto layout */
+display: flex;
+flex-direction: column;
+align-items: flex-start;
+padding: 0px;
+gap: 30px;
+
+width: 474.46px;
+height: 224px;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Watch your progress every day */
+
+width: 474.46px;
+height: 144px;
+
+/* Headings/Heading 2 */
+font-family: 'Dela Gothic One';
+font-style: normal;
+font-weight: 400;
+font-size: 48px;
+line-height: 100%;
+/* or 48px */
+letter-spacing: -0.04em;
+
+/* Primaty/Black */
+color: #000000;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
+
+
+/* Save statistics on your achievements, words learned, and mistakes */
+
+width: 396.89px;
+height: 50px;
+
+/* Text/Body 1 */
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 600;
+font-size: 18px;
+line-height: 140%;
+/* or 25px */
+letter-spacing: -0.04em;
+
+/* Primaty/Grey Dark */
+color: #757575;
+
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+
+/* Frame 6 */
+
+/* Auto layout */
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: center;
+padding: 14px 22px;
+gap: 19px;
+
+width: 129px;
+height: 44px;
+
+/* Primaty/Cyan Light */
+background: #C3DCE3;
+border-radius: 100px;
+
+/* Inside auto layout */
+flex: none;
+order: 1;
+flex-grow: 0;
+
+
+/* About platform */
+
+width: 85px;
+height: 16px;
+
+/* Button/Button 2 */
+font-family: 'Montserrat';
+font-style: normal;
+font-weight: 700;
+font-size: 16px;
+line-height: 100%;
+/* identical to box height, or 16px */
+display: flex;
+align-items: center;
+letter-spacing: -0.04em;
+
+/* Primaty/Cyan Dark */
+color: #2B788B;
+
+
+/* Inside auto layout */
+flex: none;
+order: 0;
+flex-grow: 0;
